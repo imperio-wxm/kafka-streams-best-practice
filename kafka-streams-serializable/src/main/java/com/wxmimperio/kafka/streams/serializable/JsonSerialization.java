d98b458e512101d159a1244aecdf56710733e6f2
wxm <wxmimperio@outlook.com>
2019-01-06 17:08:43 +0800

    update json serde

--- kafka-streams-serializable/src/main/java/com/wxmimperio/kafka/streams/serializable/JsonSerialization.java
+++ kafka-streams-serializable/src/main/java/com/wxmimperio/kafka/streams/serializable/JsonSerialization.java
@@ -1,4 +1,83 @@
 package com.wxmimperio.kafka.streams.serializable;
 
-public class JsonSerialization {
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.nio.charset.Charset;
+import java.util.Map;
+
+public class JsonSerialization implements Serde<JSONObject> {
+
+    @Override
+    public void configure(Map<String, ?> map, boolean b) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public Serializer<JSONObject> serializer() {
+        return new JSONSerializer();
+    }
+
+    @Override
+    public Deserializer<JSONObject> deserializer() {
+        return new JSONDeserializer();
+    }
+
+
+    static private class JSONSerializer implements Serializer<JSONObject> {
+        @Override
+        public void configure(Map<String, ?> map, boolean b) {
+
+        }
+
+        @Override
+        public byte[] serialize(String s, JSONObject jsonObject) {
+            if (jsonObject == null) {
+                return null;
+            }
+            try {
+                return jsonObject.toJSONString().getBytes(Charset.forName("UTF-8"));
+            } catch (Exception e) {
+                throw new SerializationException(String.format("Error serializing JSON message: %s", jsonObject.toJSONString()), e);
+            }
+        }
+
+        @Override
+        public void close() {
+
+        }
+    }
+
+    static private class JSONDeserializer implements Deserializer<JSONObject> {
+        @Override
+        public void configure(Map<String, ?> map, boolean b) {
+
+        }
+
+        @Override
+        public JSONObject deserialize(String s, byte[] bytes) {
+            if (bytes == null) {
+                return null;
+            }
+            try {
+                return JSON.parseObject(new String(bytes, Charset.forName("UTF-8")));
+            } catch (Exception e) {
+                throw new SerializationException(String.format("Error deserialize JSON message: %s", new String(bytes, Charset.forName("UTF-8"))), e);
+            }
+        }
+
+        @Override
+        public void close() {
+
+        }
+    }
 }
--- kafka-streams-serializable/src/main/java/com/wxmimperio/kafka/streams/streams/JsonStreams.java
+++ kafka-streams-serializable/src/main/java/com/wxmimperio/kafka/streams/streams/JsonStreams.java
@@ -1,12 +1,16 @@
 package com.wxmimperio.kafka.streams.streams;
 
+import com.alibaba.fastjson.JSONObject;
+import com.wxmimperio.kafka.streams.serializable.JsonSerialization;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 
-import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Properties;
 
 public class JsonStreams {
@@ -15,18 +19,26 @@ public class JsonStreams {
         Properties config = new Properties();
         config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
         config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.110:9092");
+        config.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "3000");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerialization.class);
+
+        DateTimeFormatter eventTimePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, byte[]> rawStreams = builder.stream("stream-test1");
-        rawStreams.filter((key,value) -> {
-            System.out.println(new String(value, Charset.forName("UTF-8")));
-            return true;
-        }).peek((key,value) -> System.out.println("Key = " + key + ", value = " + new String(value, Charset.forName("UTF-8"))));
+        final KStream<String, JSONObject> rawStreams = builder.stream(
+                "stream-test1",
+                Consumed.with(Serdes.String(),new JsonSerialization())
+        );
+
+        rawStreams.mapValues(value ->{
+            value.put("event_time",eventTimePattern.format(LocalDateTime.now()));
+            return value;
+        }).peek((key, value) ->
+                System.out.println("Key = " + key + ", value = " + value)
+        );
 
         KafkaStreams streams = new KafkaStreams(builder.build(), config);
         streams.start();
-
     }
 }
