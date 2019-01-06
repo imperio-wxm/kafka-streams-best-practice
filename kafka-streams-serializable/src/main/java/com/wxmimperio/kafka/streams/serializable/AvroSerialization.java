c0bd2dd759ee0d2d8912ef5db2d792e173dd6137
wxm <wxmimperio@outlook.com>
2019-01-07 00:50:39 +0800

    update Avro serde

--- kafka-streams-serializable/src/main/java/com/wxmimperio/kafka/streams/serializable/AvroSerialization.java
+++ kafka-streams-serializable/src/main/java/com/wxmimperio/kafka/streams/serializable/AvroSerialization.java
@@ -1,17 +1,29 @@
 package com.wxmimperio.kafka.streams.serializable;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.wxmimperio.kafka.streams.commons.SchemaUtils;
+import org.apache.avro.Schema;
+import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serializer;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Map;
 
 public class AvroSerialization  implements Serde<JSONObject> {
 
+    private Schema schema;
+
+    public AvroSerialization(Schema schema) {
+        this.schema = schema;
+    }
+
     @Override
     public void configure(Map<String, ?> configs, boolean isKey) {
-        
+
     }
 
     @Override
@@ -21,11 +33,72 @@ public class AvroSerialization  implements Serde<JSONObject> {
 
     @Override
     public Serializer<JSONObject> serializer() {
-        return null;
+        return new AvroSerializer(schema);
     }
 
     @Override
     public Deserializer<JSONObject> deserializer() {
-        return null;
+        return new AvroDeserializer(schema);
+    }
+
+    static private class AvroSerializer implements Serializer<JSONObject> {
+        private Schema schema;
+
+        public AvroSerializer(Schema schema) {
+            this.schema = schema;
+        }
+
+        @Override
+        public void configure(Map<String, ?> configs, boolean isKey) {
+
+        }
+
+        @Override
+        public byte[] serialize(String topic, JSONObject data) {
+            if(null == data) {
+                return null;
+            }
+            try {
+                return SchemaUtils.avroSerializedValue(schema, data);
+            } catch (IOException e) {
+                throw new SerializationException(String.format("Error serialize Avro message: %s", data.toJSONString()), e);
+            }
+        }
+
+        @Override
+        public void close() {
+
+        }
+    }
+
+    static private class AvroDeserializer implements Deserializer<JSONObject> {
+
+        private Schema schema;
+
+        public AvroDeserializer(Schema schema) {
+            this.schema = schema;
+        }
+
+        @Override
+        public void configure(Map<String, ?> configs, boolean isKey) {
+
+        }
+
+        @Override
+        public JSONObject deserialize(String topic, byte[] data) {
+            if(null == data) {
+                return null;
+            }
+            try {
+                return JSON.parseObject(SchemaUtils.readAvro(schema, data));
+            } catch (IOException e) {
+                throw new SerializationException(String.format("Error deserialize Avro message: %s", new String(data, Charset.forName("UTF-8"))), e);
+            }
+        }
+
+        @Override
+        public void close() {
+
+        }
     }
 }
